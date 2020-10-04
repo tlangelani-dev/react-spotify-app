1e73ba46a11883b14c19e31806a30a270ee0cb38
Tlangelani Mboweni <mbowenit@gmail.com>
2020-10-04 15:01:24 +0200

    add User component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { getTokenFromUrl } from '../spotify';
 import Login from './Login';
+import SpotifyWebApi from 'spotify-web-api-js';
+import User from './User';
+
+const spotify = new SpotifyWebApi();
 
 const App = () => {
 
-    const [token, setToken] = useState('');
+    const [token, setToken] = useState(null);
+    const [user, setUser] = useState(null);
     
     useEffect(() => {
         const _token = getTokenFromUrl();
+        window.location.hash = '';
         if (_token) {
             setToken(_token);
+            spotify.setAccessToken(_token);
+            spotify.getMe().then(user => {
+                console.log(user);
+            });
         }
-        console.log(token);
     }, []);
 
     return (
         <div className="app">
-            <Login />
+            {token ? <User /> : <Login />}
         </div>
     )
 };
